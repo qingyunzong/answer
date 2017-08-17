71a922c4c28cc71406d6f45aea76c8bd74819a5d
Joker <917275527@qq.com>
2017-08-17 17:38:06 +0800

    root

--- src/main/java/com/poi/answer/configuration/RedisCacheConfiguration.java
+++ src/main/java/com/poi/answer/configuration/RedisCacheConfiguration.java
@@ -3,12 +3,19 @@ package com.poi.answer.configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Administrator on 2017/8/17.
@@ -39,7 +46,6 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
     @Bean
     public JedisPool redisPoolFactory() {
         logger.info("JedisPool注入成功！！");
-        logger.info("redis地址：" + host + ":" + port);
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         jedisPoolConfig.setMaxIdle(maxIdle);
         jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
@@ -47,6 +53,27 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
         JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
 
         return jedisPool;
+    }
+
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(cf);
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+
+        //默认超时时间,单位秒
+        cacheManager.setDefaultExpiration(3000);
+        //根据缓存名称设置超时时间,0为不超时
+        Map<String,Long> expires = new ConcurrentHashMap<String,Long>();
+        cacheManager.setExpires(expires);
+
+        return cacheManager;
     }
 
 }
